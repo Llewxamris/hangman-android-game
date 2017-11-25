038b6a24a58e9856a1530b4ab015fd591cb728ff
Maxwell Haley <maxwell.haley@openmailbox.org>
2017-11-24 23:45:44 -0500

    Save options using SharedPreferences
    Set default options on app start. Save the options once the user hits
    the save button inside the OptionsActivity.
    
    TODO: Validate Min and Max Length values before saving.

--- app/src/main/java/ca/qc/cegep_heritage/mhaley_a03_hangman/MainActivity.java
+++ app/src/main/java/ca/qc/cegep_heritage/mhaley_a03_hangman/MainActivity.java
@@ -1,6 +1,8 @@
 package ca.qc.cegep_heritage.mhaley_a03_hangman;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -15,6 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("options", Context.MODE_PRIVATE);
+
+        if(sharedPreferences.getInt("minLength", 0) == 0) {
+            SharedPreferences.Editor editSharedPrefs = sharedPreferences.edit();
+            editSharedPrefs.putInt("minLength", 3);
+            editSharedPrefs.putInt("maxLength", 13);
+            editSharedPrefs.putInt("difficulty", R.id.rdoEasy); // rdoEasy is from activity_options
+            editSharedPrefs.apply();
+        }
+
+
+
     }
 
     @Override
--- app/src/main/java/ca/qc/cegep_heritage/mhaley_a03_hangman/OptionsActivity.java
+++ app/src/main/java/ca/qc/cegep_heritage/mhaley_a03_hangman/OptionsActivity.java
@@ -1,6 +1,8 @@
 package ca.qc.cegep_heritage.mhaley_a03_hangman;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,21 +16,32 @@ public class OptionsActivity extends AppCompatActivity {
     private EditText edtxtMinLength;
     private EditText edtxtMaxLength;
     private RadioGroup rdoGrpDifficulty;
+    private Intent mainIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
 
+        edtxtMinLength =  findViewById(R.id.edtxtMinLength);
+        edtxtMaxLength = findViewById(R.id.edtxtMaxLength);
+        rdoGrpDifficulty = findViewById(R.id.rdoGrpDifficulty);
+        mainIntent = new Intent(OptionsActivity.this, MainActivity.class);
+
+        getSharedPreferences();
+
         Button btnSave = findViewById(R.id.btnSave);
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // TODO: Validate Min and Max Length values before saving.
+                // Min should be: 3 >= Min >= Max
+                // Max should be: 3 >= Max && Min > Max
+
                 Snackbar saveSnackbar = Snackbar
-                                            .make(findViewById(R.id.layOptions),
-                                                    "Options Saved",
-                                                    Snackbar.LENGTH_LONG);
+                        .make(findViewById(R.id.layOptions),"Options Saved", Snackbar.LENGTH_LONG);
                 saveSnackbar.show();
+                setSharedPreferences();
             }
         });
 
@@ -36,7 +49,6 @@ public class OptionsActivity extends AppCompatActivity {
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent mainIntent = new Intent(OptionsActivity.this, MainActivity.class);
                 startActivity(mainIntent);
             }
         });
@@ -45,10 +57,6 @@ public class OptionsActivity extends AppCompatActivity {
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
-
-        edtxtMinLength = findViewById(R.id.edtxtMinLength);
-        edtxtMaxLength = findViewById(R.id.edtxtMaxLength);
-        rdoGrpDifficulty = findViewById(R.id.rdoGrpDifficulty);
 
         savedInstanceState.putString("minLength", edtxtMinLength.getText().toString());
         savedInstanceState.putString("maxLength", edtxtMaxLength.getText().toString());
@@ -63,4 +71,23 @@ public class OptionsActivity extends AppCompatActivity {
         edtxtMaxLength.setText(savedInstanceState.getString("maxLength"));
         rdoGrpDifficulty.check(savedInstanceState.getInt("selectedDifficulty"));
     }
+
+    private void getSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences("options", Context.MODE_PRIVATE);
+
+        edtxtMinLength.setText(String.valueOf(sharedPreferences.getInt("minLength", 3)));
+        edtxtMaxLength.setText(String.valueOf(sharedPreferences.getInt("maxLength", 13)));
+        rdoGrpDifficulty.check(sharedPreferences.getInt("difficulty", R.id.rdoEasy));
+    }
+
+    private void setSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences("options", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editSharedPrefs = sharedPreferences.edit();
+
+        editSharedPrefs.putInt("minLength", Integer.parseInt(edtxtMinLength.getText().toString()));
+        editSharedPrefs.putInt("maxLength", Integer.parseInt(edtxtMaxLength.getText().toString()));
+        editSharedPrefs.putInt("difficulty", rdoGrpDifficulty.getCheckedRadioButtonId());
+        editSharedPrefs.apply();
+    }
+
 }
