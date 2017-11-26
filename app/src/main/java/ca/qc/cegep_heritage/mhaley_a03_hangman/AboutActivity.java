961c4cb8d2303cf66f7a7c5fa4424cc0e00e8623
Maxwell Haley <maxwell.haley@openmailbox.org>
2017-11-26 16:30:01 -0500

    Rework AboutActivity layout, extract string
    Move strings and some dimensitions to their proper resource file.

--- app/src/main/java/ca/qc/cegep_heritage/mhaley_a03_hangman/AboutActivity.java
+++ app/src/main/java/ca/qc/cegep_heritage/mhaley_a03_hangman/AboutActivity.java
@@ -2,12 +2,19 @@ package ca.qc.cegep_heritage.mhaley_a03_hangman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class AboutActivity extends AppCompatActivity {
+    /* The about page. Shows the date/time */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-    }
-}
+
+        TextView txtDateTime = findViewById(R.id.txtDateTime);
+        txtDateTime.setText(new Date().toString());
+    } // onCreate(...)
+} // AboutActivity
